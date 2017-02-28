59779ddc25a9e8ffa4c8f83ca0c9600ef94b5ab7
allencompsci <Taylor_Hudson@allenisd.org>
2017-02-28 13:04:28 -0600

    f

--- Intro WinBgi/Intro WinBgi/example.cpp
+++ Intro WinBgi/Intro WinBgi/example.cpp
@@ -68,7 +68,7 @@ void main() {
 }
 void gr_Start(int &GrDriver, int&GrMode, int&ErrorCode) {
 	//set the graphics driver
-	GrDriver = VGA; //800x600 
+	GrDriver = VGA; //800x600 by monitor
 	GrMode = VGAMAX;//set graphics mode
 	initgraph(&GrDriver, &GrMode, "");//start graphics
 	//check for problems
