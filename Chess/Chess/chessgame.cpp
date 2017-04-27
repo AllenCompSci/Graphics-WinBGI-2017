4fb306316933a67b25066ef876753cc59490dd24
allencompsci <Taylor_Hudson@allenisd.org>
2017-04-27 07:09:40 -0500

    Creation of Board, setup

--- Chess/Chess/chessgame.cpp
+++ Chess/Chess/chessgame.cpp
@@ -10,27 +10,20 @@ In this project, we will be creating a chess game.
 */
 
 // Libraries
+#define _CRT_SECURE_NO_WARNINGS /// Necessary for fopen()
 #include <iostream>
 #include <ctime>
 #include <winbgi.cpp>
 #include <Windows.h>
 #include <graphics.h>
 #include <vector>
-
+#include <string>
+#include <fstream>
+#include "Pieces.h" // Contains all of the Pieces arrays
 using namespace std;
 
-// Global Variables
-int maxX, maxY;
-int GrDriver, GrMode, ErrorCode;
-
-// Pre-Defined Functions
-void gr_Start(int &GrDriver, int &GrMode, int &ErrorCode);
-void board();
-int GRID_side = 100;
-int XOffsetBOARD;
-int YOffsetBOARD;
-void MOVINGBOARD();
-enum PIECE{
+// Enums
+enum PIECE {
 	King, Queen, Bishop, Knight, Rook, Pawn, Empty
 };
 enum PIECECOLOR {
@@ -39,8 +32,66 @@ enum PIECECOLOR {
 enum PAWNSpecial {
 	UP, DOWN, NotPawn
 };
-PIECECOLOR colorBoard[8][8];
+
+// Global Variables
+int maxX, maxY;
+int GrDriver, GrMode, ErrorCode;
+const int NUMVECTOR = 3;
+int w, h;
+const int GRID_side = 100;
+int XOffsetBOARD;
+int YOffsetBOARD;
 bool Design[8][8];
+PIECECOLOR colorBoard[8][8];
+/// Color of the Pieces
+const int COLOR1 = BLACK;
+const int COLOR2 = WHITE;
+/// Color of the Board
+const int BOARD1 = RED;
+const int OTHER1 = WHITE;
+const int OTHER2 = DARKGRAY;
+// Prototyped Functions
+void setUpBoard();
+void updateChessCurrColor();
+void move(int, int, int, int);
+void every(int ROW, int COL);
+void other(int ROW, int COL);
+void gr_Start(int &GrDriver, int &GrMode, int &ErrorCode);
+void board();
+void LoadBMP();
+unsigned char* ReadBMP(char* filename);
+string toUpper(string s);
+void MOVINGBOARD();
+// Prototyped Drawing Functions for Pieces
+void drawRook(int ROW, int COL, PIECECOLOR player);
+void drawKnight(int ROW, int COL, PIECECOLOR player);
+void drawBishop(int ROW, int COL, PIECECOLOR player);
+void drawQueen(int ROW, int COL, PIECECOLOR player);
+void drawKing(int ROW, int COL, PIECECOLOR player);
+void drawPawn(int ROW, int COL, PIECECOLOR player);
+
+
+// User-defined Structures
+struct RGB {
+	int r;
+	int g;
+	int b;
+	int colorVal;
+	void init(int R, int G, int B) {
+		r = R;
+		g = G;
+		b = B;
+	}
+	void init(int R, int G, int B, int c) {
+		r = R;
+		g = G;
+		b = B;
+		colorVal = c;
+	}
+	bool isEqual(RGB t) {
+		return r == t.r && g == t.g && b == t.b;
+	}
+};
 struct boardPiece {
 	// Structures
 	PIECE currPiece;
@@ -305,11 +356,6 @@ struct boardPiece {
 			break;
 }
 }
-	void remove() {
-		currPiece = Empty;
-		currColor = NA;
-
-	}
 	void add(int dR, int dC) {
 		Radd(dR);
 		Cadd(dC);
@@ -323,16 +369,22 @@ struct boardPiece {
 	void render() {
 		switch (currPiece) {
 		case Pawn:
+			drawPawn(ROW, COL, currColor);
 			break;
 		case Knight:
+			drawKnight(ROW, COL, currColor);
 			break;
 		case Rook:
+			drawRook(ROW, COL, currColor);
 			break;
 		case Bishop:
+			drawBishop(ROW, COL, currColor);
 			break;
 		case King:
+			drawKing(ROW, COL, currColor);
 			break;
 		case Queen:
+			drawQueen(ROW, COL, currColor);
 			break;
 		case Empty:
 			remove();
@@ -340,6 +392,8 @@ struct boardPiece {
 		}
 	}
 	void remove() {
+		currPiece = Empty;
+		currColor = NA;
 		if (Design[ROW][COL]) {
 			every(ROW, COL);
 		}
@@ -348,16 +402,14 @@ struct boardPiece {
 		}
 	}
 }myboard;
-//Main
+
+// Global Variables of Struct Types
 boardPiece CHESS[8][8];
+vector <RGB> colors(NUMVECTOR);
 
-void setUpBoard();
-void updateChessCurrColor();
-void move(int, int, int, int);
-void every(int ROW, int COL);
-void other(int ROW, int COL);
+//Main
 void main() {
-
+	//LoadBMP();
 	gr_Start(GrDriver, GrMode, ErrorCode);
 
 	maxX = getmaxx(); // From Graphics
@@ -373,7 +425,6 @@ void main() {
 	system("pause");
 	return;
 }
-
 void setUpBoard() {
 	/*
 		{myboard.init(0,Rook, White), myboard.wKnight[0], myboard.wBishop[0], myboard.wQueen, myboard.wKing, myboard.wBishop[1], myboard.wKnight[1], myboard.wRook[1]},		// 1
@@ -397,7 +448,6 @@ void setUpBoard() {
 			}
 		}
 	}
-
 	CHESS[0][0].init(0, Rook, White, 0,0);
 	CHESS[7][0].init(0, Rook, Black, 7,0);
 	CHESS[0][7].init(1, Rook, White, 0,7);
@@ -414,21 +464,17 @@ void setUpBoard() {
 	CHESS[7][3].init(0, Queen, Black, 7, 3);
 	CHESS[0][4].init(0, King, White, 0, 4);
 	CHESS[7][4].init(0, King, Black, 7, 4);
-
-	/* An example of a possible move
-	CHESS[0][0].init(CHESS[0][1].index, CHESS[0][1].currPiece, CHESS[0][1].currColor, 0, 0);
-	CHESS[0][1].remove();
-	updateChessCurrColor();
-	*/
-	
-	
+	for (int i = 0; i < 8; i++) {
+		CHESS[0][i].render();
+		CHESS[1][i].render();
+		CHESS[6][i].render();
+		CHESS[7][i].render();
+	}
 }
 void MOVINGBOARD() {
 	XOffsetBOARD = (maxX - GRID_side * 8) / 2;
 	YOffsetBOARD = (maxY - GRID_side * 8) / 2;
 	bool toggle = true;
-	
-	
 	for (int j = 0; j < 8; j++) { // ROW
 		for (int i = 0; i < 8; i++) { // COL
 			if (toggle) {
@@ -475,6 +521,7 @@ void gr_Start(int &GrDriver, int&GrMode, int&ErrorCode) {
 		cout << "Error:" << ErrorCode;
 	}
 }
+///Old Board Design
 void board() {
 	// ROW 1
 	bar(0, 0, 100, 100);
@@ -557,20 +604,367 @@ void board() {
 	bar(700, 700, 800, 800);
 
 	return;
-}
+} 
 void every(int ROW, int COL) {
-	setcolor(WHITE);
+	setcolor(BOARD1);
 	bar(XOffsetBOARD + COL * GRID_side, YOffsetBOARD + ROW * GRID_side, XOffsetBOARD + COL * GRID_side + GRID_side, YOffsetBOARD + ROW * GRID_side + GRID_side);
 }
 void other(int ROW, int COL) {
-	setcolor(WHITE);
+	setcolor(OTHER1);
 	bar(XOffsetBOARD + COL * GRID_side, YOffsetBOARD + ROW * GRID_side, XOffsetBOARD + COL * GRID_side + GRID_side, YOffsetBOARD + ROW * GRID_side + GRID_side);
 	bool swatch = true;
 	for (int ii = 0; ii <=GRID_side; ii++) {
 		for (int jj = 0; jj <= GRID_side; jj++) {
 			if (swatch)
-				putpixel(XOffsetBOARD + COL * GRID_side + ii, YOffsetBOARD + ROW * GRID_side + jj, DARKGRAY);
+				putpixel(XOffsetBOARD + COL * GRID_side + ii, YOffsetBOARD + ROW * GRID_side + jj, OTHER2);
 			swatch = !swatch;
 		}
 	}
 }
+#pragma region LoadBMP
+string toUpper(string s) {
+	for (int i = 0; i < (int)s.length(); i++) {
+		if (s[i] >= 97 && s[i] <= 122)
+			s[i] -= 32;
+	}
+	return s;
+}
+unsigned char* ReadBMP(char* filename)
+{
+
+	FILE* f = fopen(filename, "rb");
+
+	if (f == NULL)
+		throw "Argument Exception";
+
+	unsigned char info[54];
+	fread(info, sizeof(unsigned char), 54, f); // read the 54-byte header
+	int width, height;
+	// extract image height and width from header
+	if (w == 0 || h == 0) {
+		width = *(int*)&info[18];
+		height = *(int*)&info[22];
+	}
+	else {
+		width = w;
+		height = h;
+	}
+	string n(filename);
+	n = n.substr(0, n.length() - 4);
+	cout << "FILE : " << n << endl;
+	cout << endl;
+	cout << "  Name: " << filename << endl;
+	cout << " Width: " << width << endl;
+	cout << "Height: " << height << endl;
+	ofstream files, paint;
+	files.open("RGB.txt");
+	int row_padded = (width * 3 + 3) & (~3);
+	unsigned char* data = new unsigned char[row_padded];
+	unsigned char tmp;
+	paint.open("paint.txt");
+	for (int i = 0; i < height; i++)
+	{
+		fread(data, sizeof(unsigned char), row_padded, f);
+		paint << "{ ";
+		for (int j = 0; j < width * 3; j += 3)
+		{
+			// Convert (B, G, R) to (R, G, B)
+			tmp = data[j];
+			data[j] = data[j + 2];
+			data[j + 2] = tmp;
+
+			files << "[" << (int)data[j] << ", " << (int)data[j + 1] << "," << (int)data[j + 2] << "] ";
+			RGB n;
+			n.init(data[j], data[j + 1], data[j + 2]);
+			bool exits = false;
+			for (int index = 0; !exits && index < (int)colors.size(); index++)
+				if (colors.at(index).isEqual(n))
+				{
+					exits = true;
+					paint << colors.at(index).colorVal;
+				}
+			if (j + 3 >= (width * 3)) {
+				paint << "}," << endl;
+			}
+			else
+				paint << ", ";
+			if (!exits)
+				colors.push_back(n);
+		}
+		files << '\n';
+	}
+	files.close();
+	paint.close();
+	files.open("vectorBuild.txt");
+	for (int i = 0; i < (int)colors.size(); i++)
+		files << "colors.at(" << i << ").init( " << colors.at(i).r << ", " << colors.at(i).g << ", " << colors.at(i).b << ", ); \n";
+	files.close();
+	vector <string> hold;
+	ifstream reverse;
+	reverse.open("paint.txt");
+	while (!reverse.eof()) {
+		string line;
+		getline(reverse, line);
+		hold.push_back(line);
+	}
+	reverse.close();
+	paint.open("paint.txt");
+	paint << "const int " << n << "_Height = " << height << "; \n";
+	paint << "const int " << n << "_Width = " << width << "; \n";
+	paint << "#pragma region " << toUpper(n) << "_DRAW \n";
+	paint << "const int " << n << "_ARRY [" << height << "][" << width << "] = { \n";
+	while (!hold.empty()) {
+		paint << hold.back() << "\n";
+		hold.pop_back();
+	}
+	paint << "}; \n \n #pragma endregion \n";
+	paint.close();
+	fclose(f);
+	return data;
+}
+void LoadBMP() {
+	w = 0;
+	h = 0;
+	
+	colors.at(2).init(255, 255, 255, (int)'W');
+	colors.at(1).init(0, 0, 0, (int)'B');
+	colors.at(0).init(252, 71, 25, 99);
+	
+	string test[6] = {
+		"Bishop", "King", "Knight", "Queen", "Rook", "Pawn"
+	};
+	for (int i = 0; i < 6; i++) {
+		string temp = "Black" + test[i] + ".bmp";
+		ReadBMP(TEXT((LPTSTR)temp.c_str()));
+		system("pause");
+		temp = "White" + test[i] + ".bmp";
+		ReadBMP(TEXT((LPTSTR)temp.c_str()));
+		system("pause");
+	}
+}
+#pragma endregion
+#pragma region drawPiece
+void drawRook(int ROW, int COL, PIECECOLOR player) {
+	int x = XOffsetBOARD + COL * GRID_side;
+	int y = YOffsetBOARD + ROW * GRID_side + 3;
+	if (player == White) {
+		for (int i = 0; i < Width; i++) {
+			for (int j = 0; j < Height; j++) {
+				switch (WhiteRook_ARRY[j][i]) {
+				case 99:
+					break;
+				case 'B':
+					putpixel(x + i, y + j, COLOR1);
+					break;
+				case 'W':
+					putpixel(x + i, y + j, COLOR2);
+					break;
+				}
+			}
+		}
+		return;
+	}
+	else {
+		for (int i = 0; i < Width; i++) {
+			for (int j = 0; j < Height; j++) {
+				switch (BlackRook_ARRY[j][i]) {
+				case 99:
+					break;
+				case 'B':
+					putpixel(x + i, y + j, COLOR1);
+					break;
+				case 'W':
+					putpixel(x + i, y + j, COLOR2);
+					break;
+				}
+			}
+		}
+	}
+}
+void drawKnight(int ROW, int COL, PIECECOLOR player) {
+	int x = XOffsetBOARD + COL * GRID_side;
+	int y = YOffsetBOARD + ROW * GRID_side + 3;
+	if (player == White) {
+		for (int i = 0; i < Width; i++) {
+			for (int j = 0; j < Height; j++) {
+				switch (WhiteKnight_ARRY[j][i]) {
+				case 99:
+					break;
+				case 'B':
+					putpixel(x + i, y + j, COLOR1);
+					break;
+				case 'W':
+					putpixel(x + i, y + j, COLOR2);
+					break;
+				}
+			}
+		}
+		return;
+	}
+	else {
+		for (int i = 0; i < Width; i++) {
+			for (int j = 0; j < Height; j++) {
+				switch (BlackKnight_ARRY[j][i]) {
+				case 99:
+					break;
+				case 'B':
+					putpixel(x + i, y + j, COLOR1);
+					break;
+				case 'W':
+					putpixel(x + i, y + j, COLOR2);
+					break;
+				}
+			}
+		}
+	}
+}
+void drawBishop(int ROW, int COL, PIECECOLOR player) {
+	int x = XOffsetBOARD + COL * GRID_side;
+	int y = YOffsetBOARD + ROW * GRID_side + 3;
+	if (player == White) {
+		for (int i = 0; i < Width; i++) {
+			for (int j = 0; j < Height; j++) {
+				switch (WhiteBishop_ARRY[j][i]) {
+				case 99:
+					break;
+				case 'B':
+					putpixel(x + i, y + j, COLOR1);
+					break;
+				case 'W':
+					putpixel(x + i, y + j, COLOR2);
+					break;
+				}
+			}
+		}
+		return;
+	}
+	else {
+		for (int i = 0; i < Width; i++) {
+			for (int j = 0; j < Height; j++) {
+				switch (BlackBishop_ARRY[j][i]) {
+				case 99:
+					break;
+				case 'B':
+					putpixel(x + i, y + j, COLOR1);
+					break;
+				case 'W':
+					putpixel(x + i, y + j, COLOR2);
+					break;
+				}
+			}
+		}
+	}
+}
+void drawQueen(int ROW, int COL, PIECECOLOR player) {
+	int x = XOffsetBOARD + COL * GRID_side;
+	int y = YOffsetBOARD + ROW * GRID_side + 3;
+	if (player == White) {
+		for (int i = 0; i < Width; i++) {
+			for (int j = 0; j < Height; j++) {
+				switch (WhiteQueen_ARRY[j][i]) {
+				case 99:
+					break;
+				case 'B':
+					putpixel(x + i, y + j, COLOR1);
+					break;
+				case 'W':
+					putpixel(x + i, y + j, COLOR2);
+					break;
+				}
+			}
+		}
+		return;
+	}
+	else {
+		for (int i = 0; i < Width; i++) {
+			for (int j = 0; j < Height; j++) {
+				switch (BlackQueen_ARRY[j][i]) {
+				case 99:
+					break;
+				case 'B':
+					putpixel(x + i, y + j, COLOR1);
+					break;
+				case 'W':
+					putpixel(x + i, y + j, COLOR2);
+					break;
+				}
+			}
+		}
+	}
+}
+void drawKing(int ROW, int COL, PIECECOLOR player) {
+	int x = XOffsetBOARD + COL * GRID_side;
+	int y = YOffsetBOARD + ROW * GRID_side + 3;
+	if (player == White) {
+		for (int i = 0; i < Width; i++) {
+			for (int j = 0; j < Height; j++) {
+				switch (WhiteKing_ARRY[j][i]) {
+				case 99:
+					break;
+				case 'B':
+					putpixel(x + i, y + j, COLOR1);
+					break;
+				case 'W':
+					putpixel(x + i, y + j, COLOR2);
+					break;
+				}
+			}
+		}
+		return;
+	}
+	else {
+		for (int i = 0; i < Width; i++) {
+			for (int j = 0; j < Height; j++) {
+				switch (BlackKing_ARRY[j][i]) {
+				case 99:
+					break;
+				case 'B':
+					putpixel(x + i, y + j, COLOR1);
+					break;
+				case 'W':
+					putpixel(x + i, y + j, COLOR2);
+					break;
+				}
+			}
+		}
+	}
+}
+void drawPawn(int ROW, int COL, PIECECOLOR player) {
+	int x = XOffsetBOARD + COL * GRID_side;
+	int y = YOffsetBOARD + ROW * GRID_side + 3;
+	if (player == White) {
+		for (int i = 0; i < Width; i++) {
+			for (int j = 0; j < Height; j++) {
+				switch (WhitePawn_ARRY[j][i]) {
+				case 99:
+					break;
+				case 'B':
+					putpixel(x + i, y + j, COLOR1);
+					break;
+				case 'W':
+					putpixel(x + i, y + j, COLOR2);
+					break;
+				}
+			}
+		}
+		return;
+	}
+	else {
+		for (int i = 0; i < Width; i++) {
+			for (int j = 0; j < Height; j++) {
+				switch (BlackPawn_ARRY[j][i]) {
+				case 99:
+					break;
+				case 'B':
+					putpixel(x + i, y + j, COLOR1);
+					break;
+				case 'W':
+					putpixel(x + i, y + j, COLOR2);
+					break;
+				}
+			}
+		}
+	}
+}
+#pragma endregion
+
