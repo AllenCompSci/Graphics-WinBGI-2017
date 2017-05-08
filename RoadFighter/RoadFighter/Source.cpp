ca6a30a911e8abeab62f76844d07429b2d4df814
allencompsci <Taylor_Hudson@allenisd.org>
2017-05-08 08:53:53 -0500

    chicken

--- RoadFighter/RoadFighter/Source.cpp
+++ RoadFighter/RoadFighter/Source.cpp
@@ -7,6 +7,7 @@ void main() {
 	int GrDriver, GrMode;
 	initgraph(&GrDriver, &GrMode, "");
 	setcolor(DARKGRAY);
+	int x = 2;
 while(true){
 	bar(0, 0, getmaxx(), getmaxy());
 
@@ -36,6 +37,15 @@ while(true){
 	bar(0, 0, getmaxx(), getmaxy());
 	draw((getmaxx() - TRUCK_Width) / 2, (getmaxy() - TRUCK_Height) / 2, BLUETRUCK);
 	getch();
+	bar(0, 0, getmaxx(), getmaxy());
+	chickenMOVE((getmaxx() - chicken_Width) / 2, (getmaxy() - chicken_Height) / 2);
+	getch();
+	bar(0, 0, getmaxx(), getmaxy());
+
+	greatChicken((getmaxx() - chicken_Width*x) / 2, (getmaxy() - chicken_Height*x) / 2, x);
+	x++;
+	getch();
+	setcolor(DARKGRAY);
 }
 	system("pause");
     
